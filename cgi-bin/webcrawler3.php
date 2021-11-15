6d7c79c5196f5c4f704421fe79270c955cd18d14
undergraduate <cse20161614@cspro.sogang.ac.kr>
2021-11-15 02:12:55 -0800

    Added Comments of webcrawler3.php

--- cgi-bin/webcrawler3.php
+++ cgi-bin/webcrawler3.php
@@ -42,28 +42,33 @@ while(1){
                 //Get Current Anchor
                 $CurAnch=$element->href;
 
-                //Echo it
+                //Echo it if needed
                 //echo $CurAnch, "<br>";
 
+                // Filter Reliable Anchors
                 if(strncmp("https://", $CurAnch, 8) == 0 || strncmp("http://", $CurAnch, 7) == 0){
                         
                         //STEP 2 : Get Current Domain
                         $CurDomain = GetDomain($CurAnch);
                         
-                        //Echo it
+                        //Echo it if needed
                         //echo($CurDomain. "<br>");
                  
+
+                        //STEP 3 : Filter And Cook
+
                         //If current Anchor's Domain is NOT Duplicate
                         if(CheckDuplicates($Domains,$CurDomain)==0){
 
-                                //echo "Non-Dup! <br>";
+                                //echo "Non-Duplicate! <br>";
 
                                 //Push it into Domain List
                                 array_push($Domains,$CurDomain);
                                 
  
-                                //Push it into Anchor List
+                                //Push it into Anchor List, since we maybe need to come back
                                 array_push($Anchors,$CurAnch);
+                                //Echo it  if needed
                                 echo $COUNT; echo ". "; echo $CurAnch.'<br>';
 
 
@@ -71,10 +76,13 @@ while(1){
                                 $url=$CurAnch;
                                 $html  =  file_get_html($url);
                                 
+                                // Like stack, we update Position
                                 $AnchPos++;
                                 
                                 //Update COUNT
                                 $COUNT++;
+
+                                //Echo if needed
                                 //echo "COUNT:";
                                 //echo ($COUNT. "<br>");
 
@@ -84,16 +92,25 @@ while(1){
                         // Do Nothing
                 }
 
+                // Escape 
                 if($COUNT==20){
                         break;
                 }
         }
-
+        // Happens when url's 1st anchor has less than 20 anchors
+        
+        // We pop, and return to latest Anchor we visited
         array_pop($Anchors);
+
+        // latest Anchor
         $AnchPos--;
+
+        //update url and html
         $url = $Anchors[$AnchPos];
         $html = file_get_html($url);
 
+
+        //Escape Condition
         if($COUNT==20){
                 break;
         }
@@ -109,7 +126,7 @@ for($i=0; $i<count($Anchors); $i++){
 */
 
 
-
+//Function To Check Duplicates
 function CheckDuplicates($Array,$Val){
         for($i=0;$i<count($Array);$i++){
                 if($Array[$i]==$Val){
@@ -119,6 +136,7 @@ function CheckDuplicates($Array,$Val){
         return 0;
 }
 
+// Function To Get Domain using parse_url
 function GetDomain($URL){
         $parsedURL = parse_url($URL);
         return $parsedURL['host'];
